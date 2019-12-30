2c0d40f8e8f6a3236990f8ab69cf5a985a54f09e
Sando <sandrinekaneuf@hotmail.com>
2019-12-30 16:32:45 +0100

    First commit

---
+++ UnityForBeginners/Assets/showAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showAnim : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnHit()
+    {
+        anim.SetTrigger("hit");
+    }
+
+
+
+}
